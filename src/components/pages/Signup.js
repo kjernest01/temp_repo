0aea336e1e115630bcfc85bc9dfa5b83b02ea49b
Samajah <117935690+Samajah@users.noreply.github.com>
2023-11-10 19:30:44 -0600

    Add files via upload

---
+++ src/components/pages/Signup.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import './src/component/pages/Signup.css'
+
+const Signup = () => {
+    return (
+        <div className='container'>
+        <div className ='header'>
+        <div className = 'text'>
+            Sign Up
+            </div>
+            <div className = 'inputs'>
+                
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
