bcf5e6a4ce9052ca157baf30fa0d18f3f2a823dc
Samajah <samajah.abburi@gmail.com>
2023-11-11 00:50:39 -0600

    Rename Logo to PriceTracker in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,7 +66,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
+            PriceTracker
             <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
